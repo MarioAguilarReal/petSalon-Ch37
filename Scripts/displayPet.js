b727c420cfff87a0053dcc352b1daabdf0ae3bfb
Mario Aguilar Real <94491813+MarioAguilarReal@users.noreply.github.com>
2023-04-24 17:16:59 -0700

    Final Report
    Final report

--- Scripts/displayPet.js
+++ Scripts/displayPet.js
@@ -1,20 +1,21 @@
 const tableBody = document.getElementById('table-body');
+const dropdown = document.getElementById('cardsDiv');
 
 
 function putBg(service) {
-    console.log(service)
+    // console.log(service)
     let color;
     if (service == 'Grooming') {
-        color = '#d34a23';
+        color = '#ec92b8';
 
     } else if (service == 'Vaccines') {
-        color = '#ab1333';
+        color = '#b900d6';
 
     } else if (service == 'Nail triming') {
-        color = '#8cc5fa';
+        color = '#9ad600';
 
     } else if (service == 'Shower') {
-        color = '#dfa133';
+        color = '#0b76a6';
 
     }
     return color;
@@ -28,10 +29,10 @@ function displayPetCards() {
         let color = "";
         let pet = petSalon.pets[i];
         color = putBg(pet.service);
-        console.log(pet)
+        //console.log(pet)
         //create template
         card += `
-        <div class="pet" style="background-color:${color}">
+        <div class="pet" id="${pet.id}" style="background-color:${color}">
             <h5>Name: ${pet.name}</h5>
             <hr>
             <p>Age: ${pet.age}</p>
@@ -55,14 +56,23 @@ function displayTable() {
         let pet = petSalon.pets[i];
         color = putBg(pet.service);
         tableInfo += `
-        <tr style="background-color:${color}">
+        <tr id="${pet.id + 'table'}" style="background-color:${color}">
             <td>${pet.name}</td>
             <td>${pet.age}</td>
             <td>${pet.gender}</td>
             <td>${pet.breed}</td>
             <td>${pet.service}</td>
+            <td onclick="deletePet(${pet.id})" id="button-delete">Delete</td>
         </tr>
             `;
     }
     tableBody.innerHTML = tableInfo;
+}
+
+function toggleDropdown() {
+    if (dropdown.style.display === 'none') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
 }
--- Scripts/register.js
+++ Scripts/register.js
@@ -21,8 +21,9 @@ let petSalon = {
 }
 
 //Create the pet name, age, gender, breed, service
-function Pets(name, age, gender, breed, service) {
-    this.name = name;
+function Pets(id, name, age, gender, breed, service) {
+    this.id = id,
+        this.name = name;
     this.age = age;
     this.gender = gender;
     this.breed = breed;
@@ -35,18 +36,54 @@ function Pets(name, age, gender, breed, service) {
 
 //Code form
 
-let inputName = document.querySelector('#name');
-let inputAge = document.querySelector('#age');
-let inputBreed = document.querySelector('#breed');
-let inputGender = document.querySelector('#gender');
-let inputService = document.querySelector('#services');
+const inputName = document.querySelector('#name');
+const inputAge = document.querySelector('#age');
+const inputBreed = document.querySelector('#breed');
+const inputGender = document.querySelector('#gender');
+const inputService = document.querySelector('#services');
+
 
 function isValid(aPet) {
     //we assume that the pet is valid
     let valid = true;
+    console.log(aPet.gender)
+    console.log(aPet.breed)
     if (aPet.name == '') {
         valid = false;
         inputName.classList.add('error');
+        setTimeout(() => {
+            inputName.classList.remove('error');
+        }, 3000)
+    }
+    if (aPet.age == '') {
+        valid = false;
+        inputAge.classList.add('error');
+        setTimeout(() => {
+            inputAge.classList.remove('error');
+        }, 3000)
+    }
+    if (aPet.gender === '') {
+        valid = false;
+        inputGender.classList.add('error');
+        console.log(inputGender);
+        setTimeout(() => {
+            inputGender.classList.remove('error');
+        }, 3000)
+    }
+    if (aPet.breed == '') {
+        valid = false;
+        inputBreed.classList.add('error');
+        console.log(inputBreed)
+        setTimeout(() => {
+            inputBreed.classList.remove('error');
+        }, 3000)
+    }
+    if (aPet.service == '') {
+        valid = false;
+        inputService.classList.add('error');
+        setTimeout(() => {
+            inputService.classList.remove('error');
+        }, 3000)
     }
     return valid;
 }
@@ -55,23 +92,24 @@ function register(e) {
     //get the values from the inputs
 
     //create the new pet
-    let newPet = new Pets(inputName.value, inputAge.value, inputBreed.value, inputGender.value, inputService.value);
+    console.log(petSalon.pets.length)
+    let newPet = new Pets(petSalon.pets.length, inputName.value, inputAge.value, inputGender.value, inputBreed.value, inputService.value);
     if (isValid(newPet)) {
         //push
         petSalon.pets.push(newPet);
-        addAlert();
+        addAlertCorrect();
         displayPetCards();
         displayTable();
         //addOnePetNum();
         //addAlert();
     } else {
-        alert('Please fill out the name');
+        addAlertError();
+        return 0;
     }
     //push the newPet into array
 
     clearInputs();
 }
-
 function clearInputs() {
     inputName.value = '';
     inputAge.value = '';
@@ -79,7 +117,44 @@ function clearInputs() {
     inputGender.value = '';
     inputService.value = '';
 }
+function alertDelete() {
 
+}
+function deletePet(id) {
+    // console.log(id)
+    let deleteIndex;
+    for (let i = 0; i < petSalon.pets.length; i++) {//traveling the array
+        let pet = petSalon.pets[i];
+        //console.log(pet.id)
+        if (id == pet.id) {//compare the id with pets id
+            deleteIndex = i;//Getting the position of the pet in the array
+            console.log(deleteIndex)
+        }
+    }
+    document.getElementById(id).remove;//remove from html
+    petSalon.pets.splice(deleteIndex, 1); //remove from the array
+    displayTable()
+    displayPetCards()
+}
+function searchPet() {
+    let searchString = document.getElementById("txtSearch").value;
+    for (let i = 0; i < petSalon.pets.length; i++) {
+        let pet = petSalon.pets[i];
+        let card = document.getElementById(pet.id);
+        let lineTable = document.getElementById(pet.id + 'table')
+        //console.log(card)
+        if (searchString.toLowerCase() != '' && (pet.name.toLowerCase().includes(searchString.toLowerCase())) || pet.service.toLowerCase().includes(searchString.toLowerCase())) {
+            //console.log((' I found it!'));
+            //card.classList.add('highlight');
+            card.style.display = "";
+            lineTable.style.display = "";
+        } else {
+            // card.classList.remove('highlight');
+            card.style.display = "none";
+            lineTable.style.display = "none";
+        }
+    }
+}
 function addOnePetNum() {
     let text = document.createElement('p');
     text.classList.add('text-num');
@@ -88,20 +163,37 @@ function addOnePetNum() {
     //text.innerHTML(`<p>${petSalon.pets.length}</p>`);
     counter.appendChild(text);
 }
-
-function addAlert() {
+function addAlertCorrect() {
     console.log(alertDiv)
-    alertDiv.innerHTML = `<div class="alert">
-    Congratulations, A Pet Was Added Successfully!
-    </div>`
-    alertDiv.classList.add('green-bar');
-}
 
+    const alertaExito = document.createElement('P');
+    alertaExito.classList.add('green-bar');
+    alertaExito.textContent = 'Congratulations, A Pet Was Added Successfully!';
+    alertDiv.appendChild(alertaExito);
+
+    setTimeout(() => {
+        alertaExito.remove();
+    }, 3000)
+}
+function addAlertError() {
+    //console.log(alertDiv)
+
+    const alertaExito = document.createElement('P');
+    alertaExito.classList.add('red-bar');
+    alertaExito.textContent = 'LLena todos los campos';
+    alertDiv.appendChild(alertaExito);
+
+    setTimeout(() => {
+        alertaExito.remove();
+    }, 3000)
+}
 function init() {
-    let camilo = new Pets('Camilo', 12, 'Male', 'German Shepherd', 'Grooming');
-    let rufi = new Pets('Rufi', 12, 'Male', 'Belgian Shepherd', 'Nail triming');
-    let lobo = new Pets('Lobo', 12, 'Male', 'Labrador', 'Shower');
-    petSalon.pets.push(camilo, rufi, lobo);
+    let camilo = new Pets(petSalon.pets.length, 'Camilo', 12, 'Male', 'German Shepherd', 'Grooming');
+    petSalon.pets.push(camilo);
+    let rufi = new Pets(petSalon.pets.length, 'Rufi', 12, 'Male', 'Belgian Shepherd', 'Nail triming');
+    petSalon.pets.push(rufi);
+    let lobo = new Pets(petSalon.pets.length, 'Lobo', 12, 'Male', 'Labrador', 'Shower');
+    petSalon.pets.push(lobo);
     displayPetCards();
     displayTable();
     //addOnePetNum();
